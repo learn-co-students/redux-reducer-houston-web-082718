55bc12c673d57f9a0e7220f20dedf1429deee66c
spraguesy <andrewsprague@outlook.com>
2018-11-19 12:57:16 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,18 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      state.friends.push(action.friend);
+      return { ...state };
+    case "REMOVE_FRIEND":
+      let newArray = [...state.friends];
+      console.log(newArray.find(friend => friend.id === action.id));
+
+      newArray.splice(
+        newArray.indexOf(newArray.find(friend => friend.id === action.id)),
+        1
+      );
+      return { friends: newArray };
+    default:
+      return { friends: state.friends };
+  }
 }
